ecabde8f2e5ea3b02128de9514a365489be63dfa
bhawana <bhawana.prs@gmail.com>
2019-12-22 06:08:13 +0545

    welcome message for new user

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const socketio = require("socket.io");
 const http = require("http");
 const PORT = process.env.PORT || 5000;
 
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./users");
+
 const router = require("./router");
 const app = express();
 const server = http.createServer(app);
@@ -11,10 +13,17 @@ const io = socketio(server);
 app.use(router);
 
 io.on("connection", socket => {
-  console.log("we have a new connection");
-
   socket.on("join", ({ name, room }, callback) => {
-    console.log(name, room);
+    const { error, user } = addUser({ id: socket.id, name, room });
+    if (error) return callback(error);
+    socket.emit("message", {
+      user: "admin",
+      text: `${user.name}, welcome to the room`
+    });
+    socket.broadcast
+      .to(user.room)
+      .emit("message", { user: "admin", test: `${user.name} has joined` });
+    socket.join(user.room);
   });
 
   socket.on("disconnect", () => {
