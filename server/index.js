c6982036c2b21c3e8c0fce2c71bca01078618264
bhawana <bhawana.prs@gmail.com>
2019-12-22 15:56:14 +0545

    remove users from room

--- server/index.js
+++ server/index.js
@@ -16,28 +16,52 @@ io.on("connection", socket => {
   socket.on("join", ({ name, room }, callback) => {
     console.log(room);
     const { error, user } = addUser({ id: socket.id, name, room });
+
     if (error) return callback(error);
     socket.emit("message", {
       user: "admin",
       text: `${user.name}, welcome to the room`
     });
+
     socket.broadcast
       .to(user.room)
       .emit("message", { user: "admin", test: `${user.name} has joined` });
     socket.join(user.room);
+
+    io.to(user.room).emit("roomData", {
+      user: user.room,
+      users: getUsersInRoom(user.room)
+    });
+
     callback();
   });
 
   socket.on("sendMessage", (message, callback) => {
     const user = getUser(socket.id);
-    console.log(user);
-    io.to(user.room).emit("message", { user: user.room, text: message });
+
+    io.to(user.room).emit("roomData", {
+      user: user.room,
+      users: getUsersInRoom(user.room)
+    });
+
+    io.to(user.room).emit("message", { user: user.name, text: message });
 
     callback();
   });
 
   socket.on("disconnect", () => {
-    console.log("user has left");
+    const user = removeUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit("message", {
+        user: "Admin",
+        text: `${user.name} has left.`
+      });
+      io.to(user.room).emit("roomData", {
+        room: user.room,
+        users: getUsersInRoom(user.room)
+      });
+    }
   });
 });
 
